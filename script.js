6763fec31f963c19a5a11dcc13b3f2e026ad3da2
yog980 <bhattyog1997@gmail.com>
2023-04-17 17:58:47 +0100

    script file added

---
+++ script.js
@@ -0,0 +1,39 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+let circleTurn
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    //Check for win
+    //Check for Draw
+    //Switch Turns
+    swapTurns()
+    setBoardHoverClass()
+    
+}
+
+function placeMark(cell, currentClass) {
+cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
